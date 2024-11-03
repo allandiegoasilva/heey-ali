67c22b79b9347099cd0a6988c8b6b75e5ad3e2d4
Allan Diego <allandiego252@gmail.com>
2024-11-03 14:04:31 -0300

    chore: add default configurations for standard project

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+/// <reference types="vitest" />
+import { defaultExclude, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: [...defaultExclude],
+    setupFiles: ["dotenv/config"],
+  },
+});
