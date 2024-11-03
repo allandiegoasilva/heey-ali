4b64f893d9eb57eed71ba12011e5eda1f0052bba
Allan Diego <allandiego252@gmail.com>
2024-11-03 14:12:57 -0300

    feat: add ali express product endpoints

---
+++ src/enums/product-endpoint.ts
@@ -0,0 +1,7 @@
+export enum ProductEndpoint {
+  GET = 'aliexpress.ds.product.get',
+  CATEGORY = 'aliexpress.ds.category.get',
+  FEEDNAME = 'aliexpress.ds.feedname.get',
+  FEED_ITEM_IDS = 'aliexpress.ds.feed.itemids.get',
+  RECOMMENDED = 'aliexpress.ds.recommend.feed.get',
+}
