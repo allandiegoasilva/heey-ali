e8aed1223c16daa35fb7b53ad89bd8aef0e29c1a
Allan Diego <allandiego252@gmail.com>
2026-01-20 23:40:11 -0300

    chore: add path alias resolution in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 /// <reference types="vitest" />
+import path from "path";
 import { defaultExclude, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     exclude: [...defaultExclude],
     setupFiles: ["dotenv/config"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
