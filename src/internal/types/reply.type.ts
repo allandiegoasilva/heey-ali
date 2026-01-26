0336162c35f55040c44594f23fdc9ef55a453b67
Allan Diego <allandiego252@gmail.com>
2026-01-26 00:27:29 -0300

    feat: extend ReplyDto to include HeeyAliGenericError for enhanced error response handling

--- src/internal/types/reply.type.ts
+++ src/internal/types/reply.type.ts
@@ -1,5 +1,7 @@
+import { HeeyAliGenericError } from './heey-ali-generic-error.type';
+
 export type ReplyDto<T = void> = {
   code: number;
   success: boolean;
-  data?: T;
+  data?: T | HeeyAliGenericError;
 };
