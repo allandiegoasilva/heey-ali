b4dade84b47ec41957177b7c438862b8249ffcda
Allan Diego <allandiego252@gmail.com>
2026-01-25 22:47:56 -0300

    fix: update heeyAliGetCredentialMock to include redirectUri and improve error message for missing

--- test/integration/mocks/heey-ali-get-credential.mock.ts
+++ test/integration/mocks/heey-ali-get-credential.mock.ts
@@ -1,11 +1,14 @@
-export function heeyAliGetCredentialMock() {
-  if(!process.env.ALI_EXPRESS_APP_KEY || !process.env.ALI_EXPRESS_APP_SECRET || !process.env.ALI_EXPRESS_BASE_URL) {
-    throw new Error('ALI_EXPRESS_APP_KEY, ALI_EXPRESS_APP_SECRET e ALI_EXPRESS_BASE_URL for heey ali integration tests are required.');
+import { HeeyAliCredential } from "@/types";
+
+export function heeyAliGetCredentialMock(): HeeyAliCredential & { redirectUri: string } {
+  if(!process.env.ALI_EXPRESS_APP_KEY || !process.env.ALI_EXPRESS_REDIRECT_URI || !process.env.ALI_EXPRESS_APP_SECRET || !process.env.ALI_EXPRESS_BASE_URL) {
+    throw new Error('ALI_EXPRESS_APP_KEY, ALI_EXPRESS_REDIRECT_URI, ALI_EXPRESS_APP_SECRET e ALI_EXPRESS_BASE_URL for heey ali integration tests are required.');
   }
 
   return {
     appKey: process.env.ALI_EXPRESS_APP_KEY,
     appSecret: process.env.ALI_EXPRESS_APP_SECRET,
     baseUrl: process.env.ALI_EXPRESS_BASE_URL,
+    redirectUri: process.env.ALI_EXPRESS_REDIRECT_URI,
   };
 }
