6ea34a11637aa6e1436ee2ac0282295a02a52581
Allan Diego <allandiego252@gmail.com>
2026-01-25 23:51:46 -0300

    feat: add support for conditional method inclusion based on EndpointPrefix in HeeyAliBuildParams

--- src/internal/http/builder/heey-ali-build-params.ts
+++ src/internal/http/builder/heey-ali-build-params.ts
@@ -1,18 +1,26 @@
+import { EndpointPrefix } from '@/internal/enum/endpoint-prefix';
+
 type Input = {
   appKey: string;
   method: string;
   params: Record<string, string>;
+  prefix: EndpointPrefix;
 };
 
 /* @internal */
 export class HeeyAliBuildParams {
   static build(input: Input): Record<string, string> {
-    return {
+    const params: Record<string, string> = {
       ...input.params,
-      method: input.method,
       sign_method: 'sha256',
       app_key: input.appKey,
       timestamp: Date.now().toString(),
     };
+
+    if (input.prefix == EndpointPrefix.SYNC) {
+      params['method'] = input.method;
+    }
+
+    return params;
   }
 }
