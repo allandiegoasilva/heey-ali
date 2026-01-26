98de8af8a8b9de637514d422ed6431a9a8b9a17b
Allan Diego <allandiego252@gmail.com>
2026-01-25 22:21:23 -0300

    feat: make params optional and include prefix in HeeyAliHttpClientRequest type

--- src/internal/types/heey-ali-http-client-request.type.ts
+++ src/internal/types/heey-ali-http-client-request.type.ts
@@ -1,8 +1,10 @@
 import { HttpMethod } from '@/internal/enum/http-method';
+import { EndpointPrefix } from '../enum/endpoint-prefix';
 
 /* @internal */
 export type HeeyAliHttpClientRequest = {
   method: HttpMethod;
+  prefix: EndpointPrefix;
   endpoint: string;
-  params: Record<string, string>;
+  params?: Record<string, string>;
 };
